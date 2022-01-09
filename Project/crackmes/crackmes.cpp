a8c35b0295f428b0c1bdfe90d8022e37d8662198
Paradoxxs <Aston.dk@gmail.com>
2022-01-09 17:54:06 +0100

    Add files via upload

---
+++ Project/crackmes/crackmes.cpp
@@ -0,0 +1,49 @@
+
+#include <string>
+#include <iostream> 
+#include <fstream>
+#include <Windows.h>
+
+using namespace std;
+
+
+
+int main() {
+    string content;
+    fstream file;
+    string var_1 = "zk~blcdnox";
+	char *var_2;
+
+    if (IsDebuggerPresent())
+    {
+        printf("Nope");
+        return 1;
+    }
+
+    
+	for (int cnt = 0; cnt < var_1.size(); cnt++) {
+		var_1[cnt] = var_1[cnt] ^ 10;
+	}
+
+    //std::cout << var_1 << "\n";
+
+
+	file.open("test.txt");
+	if (!file) 
+	{
+		printf("Bad1");
+		return 1;
+	}
+    file >> content;
+    //std::cout << content;
+
+
+	if(content == var_1)
+	{
+		printf("Good job!\n");
+		return 0;
+	} else {
+		printf("Bad2");
+		return 1;
+	}
+}
