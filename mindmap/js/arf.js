de85c840880ed27cc096599a3d7488d1620b844b
Paradoxxs <Aston.dk@gmail.com>
2022-05-25 13:47:06 +0200

    Update arf.js

--- mindmap/js/arf.js
+++ mindmap/js/arf.js
@@ -1,6 +1,6 @@
 var margin = [20, 120, 20, 140],
     width = 1280 - margin[1] - margin[3],
-    height = 800 - margin[0] - margin[2],
+    height = 920 - margin[0] - margin[2],
     i = 0,
     duration = 1250,
     root;
